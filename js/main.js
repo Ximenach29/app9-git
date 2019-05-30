84626d99bd237b00dd474fa87098e4c132abef1f
Ximena Contreras Hernandez <chimeena29@gmail.com>
2019-05-30 14:25:30 -0500

    4

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ var audio;
 //Hide Pause Initially
 $('#pause').hide();
 
-//Initializar-Play First Song
+//Initializer-Play First Song
 initAudio($('#playlist li:first-child'));
 
 function initAudio(element){
-	var song= element.attr('song');
-	var title= element.text();
+	var song = element.attr('song');
+	var title = element.text();
 	var cover = element.attr('cover');
 	var artist = element.attr('artist');
 	
@@ -16,79 +16,76 @@ function initAudio(element){
 	audio = new Audio('media/' + song);
 	
 	if(!audio.currentTime){
-	$('#duration').html('0.00');
+		$('#duration').html('0.00');
 	}
 	
 	$('#audio-player .title').text(title);
-	$('#audio-player .artist').text(title);
+	$('#audio-player .artist').text(artist);
 	
 	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
 	$('#playlist li').removeClass('active');
 	element.addClass('active');
-	}
-	
-	
-	//Play Button
-	$('#play').click(function (){
-		audio.play ();
-		$('#play').hide();
-		$('#pause').show();
-		$('#duration').fadeln(400);
-		showDuration();
-	});
-	
-	//Pause Botton
-	$('#pause').click(function (){
-		audio.pause ();
-		$('#play').hide();
-		$('#pause').show();
-	});
-	
-	//Stop Botton
-	$('#stop').click(function (){
-		audio.pause ();
-		audio.currentTime=0;
-		$('#pause').hide();
-		$('#play').show();
-		$('#duration').fadeOut(400);
-	});
-	
-		//PlayList Song Click
-	$('#playlist li').click(function (){
-		audio.pause ();
-		initAudio($(this));
-		$('#play').hide();
-		$('#pause').show();
-		$('#duration').fadeln(400);
-		audio.play();
-		showDuration();
-	});
-	
-	//Volume Control
-	$('#volume').change(function (){
-		audio.volume =parseFloat(this.value/10);
-	});
+}
+
+
+//Play Button
+$('#play').click(function(){
+	audio.play ();
+	$('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn(400);
+	showDuration();
+});
+
+//Pause Button
+$('#pause').click(function (){
+	audio.pause ();
+	$('#pause').hide();
+	$('#play').show();
+});
+
+//Stop Button 
+$('#stop').click(function (){
+	audio.pause ();
+	audio.currentTime = 0;
+	$('#pause').hide();
+	$('#play').show();
+	$('#duration').fadeOut(400);
+});
+
+//Playlist Song Click
+$('#playlist li').click(function (){
+	audio.pause ();
+	initAudio($(this));
+	$('#pause').hide();
+	$('#play').show();
+	$('#duration').fadeIn(400);
+audio.play();
+showDuration();
+});
+
+//Volume Control
+$('#volume').change(function (){
+	audio.volume = parseFloat(this.value / 10);
+});
 	
 	//Time Duration
-	function showDuration(){
-		$(audio).bind('timeupdate', function(){
-			//Get hours and minutes
-			var s = parseInt(audio.currentTime%60);
-			var m = parseInt((audio.currentTime/60)%60);
-			//Add 0 if seconds less than 10
-			if(s< 10){
-				s ='0' +s;
+	function showDuration (){
+		$(audio).bind('timeupdate',function (){
+			//Get hours and minutes 
+			var s = parseInt(audio.currentTime % 60);
+			var m = parseInt((audio.currentTime / 60)% 60);
+			//Add 0 of seconds less than 10
+			if (s < 10){
+				s= '0' + s;
 			}
-			$('#duration').html(m+'.'+s);
-			var value=0;
-			if(audio.currentTime>0){
-				value=Math.floor((100/ audio.duration)* audio.currentTime);
+			$('#duration').html(m + '.' + s);
+			var value = 0;
+			if (audio.currentTime > 0) { 
+			value = Math.floor((100/audio.duration)*audio.currentTime);
 			}
 			$('#progress').css('width',value+'%');
 		});
 	}
-	
-		
-	
